0dbf6f7d80b7f14fe7246f8aaa0b2c7af6e3fbb7
JayThaGreat <maskdbandit@gmail.com>
2015-11-20 16:08:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  def sit
+    puts "The Dog is sitting"
+  end
+  def name
+  end
+  def name=(@name)
+    @name
+  end
+  def breed
+  end
+  def breed=(@breed)
+    @breed
+  end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+  def job
+  end
+  def job=(@job)
+    @job
+  end
+end
