829d0a9f86c54e98d685e1f300daf953b6c3b55f
JayThaGreat <maskdbandit@gmail.com>
2015-11-20 16:12:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,14 +6,16 @@ class Dog
     puts "The Dog is sitting"
   end
   def name
-  end
-  def name=(@name)
     @name
   end
-  def breed
+  def name=(new_name)
+    @name = new_name
   end
-  def breed=(@breed)
+  def breed
     @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
   end
 end
 
