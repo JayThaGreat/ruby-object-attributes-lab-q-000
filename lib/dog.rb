fb30118d24605ad9241534ba6e720b723b8e3fdd
JayThaGreat <maskdbandit@gmail.com>
2015-11-25 14:10:11 +0000

    nitrous-backup at Wed Nov 25 14:10:11 UTC 2015

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,16 +6,12 @@ class Dog
     puts "The Dog is sitting"
   end
   def name
+  end
+  def name=(@name)
     @name
   end
-  def name=(new_name)
-    @name = new_name
-  end
   def breed
-    @breed
   end
-  def breed=(new_breed)
-    @breed = new_breed
-  end
+  def breed=(@breed)
 end
 
